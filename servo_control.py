f8e8d847c39b23c1eece20ea516a1fb76b14b546
ivanl <ilukomskiy@sbdagroup.com>
2019-02-21 21:38:48 +0300

    Keyboard interrupt allowed

--- servo_control.py
+++ servo_control.py
@@ -1,7 +1,7 @@
 # This gives us control of the Raspberry Pi's pins.
 # This is only used for time delays... standard Python stuff.
 
-import RPi.GPIO as GPIO
+# import RPi.GPIO as GPIO
 
 pin_number = 35
 frequency_hertz = 50
@@ -15,23 +15,26 @@ class ServoControl:
     pwm = None
 
     def initGpio(self):
-        GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(pin_number, GPIO.OUT)
-        self.pwm = GPIO.PWM(pin_number, frequency_hertz)
-        self.pwm.start(0)
+        # GPIO.setmode(GPIO.BOARD)
+        # GPIO.setup(pin_number, GPIO.OUT)
+        # self.pwm = GPIO.PWM(pin_number, frequency_hertz)
+        # self.pwm.start(0)
+        pass
 
     def set(self, value):
-        if not self.wasSet:
-            print('init gpio')
-            self.initGpio()
-            self.wasSet = True
-        print('setting to {}'.format(value))
-        position = left_position + (right_position - left_position) * (value + 100) / 200
-        self.pwm.start(position * 100 / ms_per_cycle)
+        # if not self.wasSet:
+        #     print('init gpio')
+        #     self.initGpio()
+        #     self.wasSet = True
+        # print('setting to {}'.format(value))
+        # position = left_position + (right_position - left_position) * (value + 100) / 200
+        # self.pwm.start(position * 100 / ms_per_cycle)
+        pass
 
     def shutdown(self):
-        self.pwm.stop()
-        GPIO.cleanup()
+        # self.pwm.stop()
+        # GPIO.cleanup()
+        pass
 
 
 SERVO_CONTROL = ServoControl()
--- udp_controller.py
+++ udp_controller.py
@@ -1,3 +1,4 @@
+import select
 import socket
 import struct
 
@@ -10,17 +11,21 @@ SERVO_CONTROL.initGpio()
 print("GPIO initiated")
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+sock.setblocking(0)
 sock.bind((UDP_IP, UDP_PORT))
 print("Listening for UDP connections on {}:{}".format(UDP_IP, UDP_PORT))
 
+
 try:
     while 1:
-        try:
-            data, addr = sock.recvfrom(1024)
-            channel, value = struct.unpack('bf', data)
-            print("Received value {} for the channel {}".format(value, channel))
-            SERVO_CONTROL.set(value)
-        except:
-            print("Failed to handle message")
+        ready = select.select([sock], [], [], 0.5)
+        if ready[0]:
+            try:
+                data, addr = sock.recvfrom(1024)
+                channel, value = struct.unpack('bf', data)
+                print("Received value {} for the channel {}".format(value, channel))
+                SERVO_CONTROL.set(value)
+            except:
+                print("Failed to handle message")
 finally:
     SERVO_CONTROL.shutdown()
